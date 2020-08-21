09fb42b3bc4eb78b7808ad5b8584abea493de7ff
Julien Cummings-Ross <julien.cummings-ross@neosolution.ca>
2020-08-20 20:55:13 -0400

    feat: keycloak authentication

--- index.js
+++ index.js
@@ -2,8 +2,34 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+var session = require("express-session");
+var Keycloak = require("keycloak-connect");
+
+var memoryStore = new session.MemoryStore();
+var keycloak = new Keycloak({ store: memoryStore });
+
+app.use(
+  session({
+    secret: "some secret",
+    resave: false,
+    saveUninitialized: true,
+    store: memoryStore,
+  })
+);
+
+app.use(
+  keycloak.middleware({
+    logout: "/logout",
+    admin: "/",
+  })
+);
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.get("/protected", keycloak.protect(), (req, res) => {
+  res.send("Protected!");
 });
 
 app.listen(port, () => {
